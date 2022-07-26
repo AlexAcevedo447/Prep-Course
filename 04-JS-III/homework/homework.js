37d1bb60549ad2e9d2cca3510545a6c6584d3ceb
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2022-07-25 21:21:20 -0500

    Solving in progress

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,23 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length - 1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+
+  return array.length;
 }
 
 
@@ -23,6 +28,12 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+    var numeros = [];
+    for( var i = 0; i < array.length; i++){
+      numeros.push(array[i] + 1);
+    }
+
+    return numeros;
 }
 
 
@@ -30,6 +41,10 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+
+  array.push(elemento)
+
+  return array;
 }
 
 
@@ -38,6 +53,10 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  
+  array.unshift(elemento);
+
+  return array;
 }
 
 
@@ -47,6 +66,16 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  var frase = "";
+  for(i = 0; i < palabras.length; i++){
+    if(i == palabras.length - 1){
+      frase += palabras[i] + "";
+    }else{
+      frase += palabras[i] + " ";
+    }
+  }
+
+  return frase;
 }
 
 
@@ -54,6 +83,14 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+
+  var found = array.find(elemento);
+
+  if(found !== undefined){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 
@@ -61,6 +98,12 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  acumulado = 0;
+  for(i = 0; i < numeros.length; i++){
+    acumulado = acumulado + array[i];
+  }
+
+  return acumulado;
 }
 
 
@@ -68,6 +111,17 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  let acumulador = 0;
+  let contador = 0;
+  let resultado = 0;
+  for(var i = 0; i < resultadosTest.length; i++){
+    acumulador = acumulado + resultadosTest[i];
+    contador = contador +1;
+  }
+
+  resultado = acumulador / contador;
+
+  return resultado;
 }
 
 
